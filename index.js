79f4f29b57cf8f5c4aae193fd292727d3496f67d
tzhasansohan <sohantajbiul210@gmail.com>
2023-06-07 21:43:22 +0400

    all users find apip done

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+
+require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
@@ -31,27 +34,74 @@ const client = new MongoClient(uri, {
   },
 });
 
+// verify jwt
+const verifyJwt = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({
+      error: true,
+      message: "unauthorized access/ need bearer authorization code",
+    });
+  }
+  const token = authorization.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({
+        error: true,
+        message: "unauthorized access failed to decoded",
+      });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     client.connect();
- const usersCollection = client
-   .db("summer-camp-project")
+
+    // jwt
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1d",
+      });
+      res.send({ token });
+    });
+
+    const usersCollection = client
+      .db("summer-camp-project")
       .collection("usersCollection");
-    
+
     // Post user data from ragistration page
-    app.post('/users', async (req, res) => {
-      
+    app.post("/users", async (req, res) => {
+      const data = req.body;
+      const existingUser = await usersCollection.findOne({
+        email: data.email,
+      });
+      if (existingUser) {
+        return res.status(400).send({ error: "User already exists" });
+      }
+      const result = await usersCollection.insertOne(data);
+      res.send(result);
+    });
+    // get user list from admin dashboard
+    app.get('/usersFromAdmin/users', async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
     })
 
-
-
+  // make Admin and instractors from admin dashboard
+    app.patch('/changeUserRole/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query)
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log(
-      "successfully connected to MongoDB!"
-    );
+    console.log("successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
   }
